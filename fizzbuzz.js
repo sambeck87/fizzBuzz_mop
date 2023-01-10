e124c597bba941f5ba3dafd6a64777aa6f2e55fc
sambeck87 <sandro_mipb@hotmail.com>
2023-01-10 08:34:15 -0600

    Make suggested changes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,13 @@
 module.exports = function () {
   const arr = [];
   for (number = 1; number <= 100; number++) {
-    if (number % 3 === 0) {
-      if (number % 5 === 0) {
+    if (number % 3 === 0 && number % 5 === 0) {
         arr.push('FizzBuzz');
         continue;
-      } else {
-        arr.push('Fizz');
-        continue;
-      }
-    } else if(number % 5 === 0){
+    } else if(number % 3 === 0){
+      arr.push('Fizz');
+      continue;
+    }else if(number % 5 === 0){
       arr.push('Buzz');
       continue;
     }
